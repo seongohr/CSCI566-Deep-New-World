9b9f63b22bf84b86c3d566a519457162ca41ff75
Angel Ricardo Nieto García <angelricardoh@gmail.com>
2020-11-28 18:55:28 -0800

    Updates

---
+++ precache-manifest.8d75b568d49f56fdfe465213356e839d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c07146361baea221e1f2351eb80cfaf",
+    "url": "/csci566-Project/index.html"
+  },
+  {
+    "revision": "b4e11ac21255e7ee3257",
+    "url": "/csci566-Project/static/css/2.0161871a.chunk.css"
+  },
+  {
+    "revision": "0f473bd16c0f2d76151b",
+    "url": "/csci566-Project/static/css/main.7f6d0bd3.chunk.css"
+  },
+  {
+    "revision": "b4e11ac21255e7ee3257",
+    "url": "/csci566-Project/static/js/2.17bf4006.chunk.js"
+  },
+  {
+    "revision": "0f473bd16c0f2d76151b",
+    "url": "/csci566-Project/static/js/main.908080d0.chunk.js"
+  },
+  {
+    "revision": "2db411bea866aa14122e",
+    "url": "/csci566-Project/static/js/runtime~main.3dd0aa38.js"
+  }
+]);
